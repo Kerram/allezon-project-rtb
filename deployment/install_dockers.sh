9ef48bf69d0ccecd83a42cc5df32fc7b3d5ca881
Marek Żochowski <marrkzochowski2@gmail.com>
2022-07-26 20:09:21 +0200

    Add deployment files

---
+++ deployment/install_dockers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -v
+
+USERNAME=$1
+PASSWORD=$2
+
+sudo apt -y install ansible sshpass
+
+for i in `seq -w 01 10`; do sshpass -p ${PASSWORD} ssh ${USERNAME}@${USERANME}vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true"; done
+
+ansible docker -m ping --extra-vars "ansible_user=${USERNAME} ansible_password=${PASSWORD}"
+
+cp ansible_hosts /etc/ansible/hosts
+cat docker_service > docker.service
+
+ansible-playbook --extra-vars "ansible_user=${USERNAME} ansible_password=${PASSWORD}" docker-playbook.yaml
+
+sudo docker info
+
+
--- run_main.sh
+++ run_main.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 
 source venv/bin/activate
 uvicorn src.main:app --reload
