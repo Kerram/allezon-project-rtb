76105da9e5e9eea2e3a5b20ca2530ce2c3f3d296
Marek Żochowski <marrkzochowski2@gmail.com>
2022-07-26 20:16:22 +0200

    Fix install dockers script

--- deployment/install_dockers.sh
+++ deployment/install_dockers.sh
@@ -5,15 +5,18 @@ PASSWORD=$2
 
 sudo apt -y install ansible sshpass
 
-for i in `seq -w 01 10`; do sshpass -p ${PASSWORD} ssh ${USERNAME}@${USERANME}vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true"; done
+for i in `seq -w 01 10`; do sshpass -p ${PASSWORD} ssh ${USERNAME}@${USERNAME}vm1$i.rtb-lab.pl -o StrictHostKeyChecking=no -C "/bin/true"; done
 
 ansible docker -m ping --extra-vars "ansible_user=${USERNAME} ansible_password=${PASSWORD}"
 
-cp ansible_hosts /etc/ansible/hosts
+sudo cp ansible_hosts /etc/ansible/hosts
 cat docker_service > docker.service
 
 ansible-playbook --extra-vars "ansible_user=${USERNAME} ansible_password=${PASSWORD}" docker-playbook.yaml
 
 sudo docker info
+DOCKER_HOST=”<username>vm102.rtb-lab.pl” docker info
+
+ansible docker -m ping --extra-vars "ansible_user=${USERNAME} ansible_password=${PASSWORD}"
 
 
